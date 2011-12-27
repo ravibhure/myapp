520cd1bf1d3bc6ce64157b11af2b1ad0166963c2
Ravi <ravibhure@gmail.com>
2011-12-27 16:24:01 +0530

    Update pgversion.php

--- pgversion.php
+++ pgversion.php
@@ -2,7 +2,7 @@
 // @Ravi.Bhure@symphonysv.com
 include('config/db.php');
  // attempt a connection
-$dbh = pg_connect("host=$hostname_DB dbname=$database_DB user=$username_DB password=$password_DB");
+$dbh = pg_connect("host=$hostname_DB dbname=postgres user=$username_DB password=$password_DB");
  if (!$dbh) {
      die("Error in connection: " . pg_last_error());
  }
